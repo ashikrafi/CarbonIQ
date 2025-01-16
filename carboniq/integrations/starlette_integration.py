6908f69370fd9e21b7e82f3185077bcb80e2204e
kow-ai-white <ashikur@thekowcompany.com>
2025-01-16 23:55:17 +0600

    Project Initialization

---
+++ carboniq/integrations/starlette_integration.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+import sys
+import threading
+
+import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+from carboniq.decorators import track_emissions
+
+app = Starlette(debug=True)
+
+
+@app.route(f"/home", methods=["GET"])
+@track_emissions(real_time=True)
+async def home(request):
+    return JSONResponse({"message": "Welcome to CarbonIQ with Starlette!"})
+
+async def main():
+    """
+    Entry point for the server.
+    """
+    port = int(os.environ.get("PORT", 8009))
+    server_thread = threading.Thread(
+        target=uvicorn.run,
+        args=(app,),
+        kwargs={"host": "0.0.0.0", "port": port},
+    )
+    server_thread.start()
+
+    if "serve" in sys.argv:
+        production_port = 80
+        port = int(os.environ.get("PORT", production_port))
+        server_thread = threading.Thread(
+            target=uvicorn.run,
+            args=(app,),
+            kwargs={"host": "0.0.0.0", "port": port},
+        )
+        server_thread.start()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
