6e982f2e3f43cf60aa1da9c3b985bbd006e91252
kow-ai-white <ashikur@thekowcompany.com>
2025-01-17 01:24:02 +0600

    Emission Logic Updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CarbonIQ",
-    version="0.1.0",
+    version="0.1.2",
     author="Md Ashikur Rahman",
     author_email="mdashikur.rafi@gmail.com",
     description="A Python package to track carbon emissions for scripts and applications.",
