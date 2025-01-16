9bd1fcc5b80e815688468ac424df11aa031c6456
kow-ai-white <ashikur@thekowcompany.com>
2025-01-17 02:09:46 +0600

    Package Updated

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="CarbonIQ",
-    version="0.1.4",
+    version="0.1.5",
     author="Md Ashikur Rahman",
     author_email="mdashikur.rafi@gmail.com",
-    description="A Python package to track carbon emissions for scripts and applications.",
+    description="A Python package that monitors and tracks the carbon emissions of scripts and applications. It provides insights into the environmental impact of code by analyzing resource usage such as CPU, memory, disk I/O, network, and GPU utilization.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ashikrafi/CarbonIQ",
